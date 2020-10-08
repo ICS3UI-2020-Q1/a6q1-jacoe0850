b5ac631d0ad41fb9f6da50b82046f7a6c7981914
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-08 16:57:18 +0000

    ACTUAL finished array heights

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 /**
- *
+ * Asks for amount of heights, acalculates average and says which numbers are above average
  * @jacoe0850 
  */
 public class Main {
@@ -10,20 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+    // scanner for input
     Scanner input = new Scanner(System.in);
+    // asking for amount heights
     System.out.println("How many heights would you like to enter");
+    // variable for amount of heights
     int arraynum = input.nextInt();
+    // array variable for amount of heights
     int[] height = new int[arraynum];
+    // ask for heights
     System.out.println("Please enter the heights on separate lines");
+    // for loop letting the user input x amounts of heights
      for(int i = 0; i < arraynum; i++){
        height[i] = input.nextInt();
      }
+     // variable for total for calculating average
      int total = 0;
+     // adds total
      for(int i = 0; i < arraynum; i++){
       total = total + height[i];
     }
+    // calculates average
     int average = total/arraynum;
+    // displays average
     System.out.println("The average height is " + average);
+    // states heights above average and calculates which heights are above average
     System.out.println("The heights above average are");
    for(int i = 0; i < height.length; i++){
      if(height[i] > average){
