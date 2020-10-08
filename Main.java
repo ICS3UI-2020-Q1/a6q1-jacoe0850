8bccc9e8fd433f3d7f3d7a05330624516cfb3760
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-08 16:43:08 +0000

    Finished array heights

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many heights would you like to enter");
+    int arraynum = input.nextInt();
+    int[] height = new int[arraynum];
+    System.out.println("Please enter the heights on separate lines");
+     for(int i = 0; i < arraynum; i++){
+       height[i] = input.nextInt();
+     }
+     int total = 0;
+     for(int i = 0; i < arraynum; i++){
+      total = total + height[i];
+    }
+    int average = total/arraynum;
+    System.out.println("The average height is " + average);
+    System.out.println("The heights above average are");
+   for(int i = 0; i < height.length; i++){
+     if(height[i] > average){
+       System.out.println(height[i]);
+     }
+   }
   }
 }
